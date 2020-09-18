fcbad43f4e2b44740162574e3c8cee010559c3c1
Pranav Jain <pranavj1001@gmail.com>
2020-09-19 00:19:08 +0530

    handle route handle handling using react-router

--- client/react-todo/src/components/home-tile/home-tile.js
+++ client/react-todo/src/components/home-tile/home-tile.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './home-tile.css';
+import { Link } from "react-router-dom";
 
 class HomeTile extends Component {
     render() {
         return (
-            <div className="home-tile--main-div">
-                <p className="home-tile--paragraph home-tile--paragraph-heading">{this.props.data.title}</p>
-                <p className="home-tile--paragraph home-tile--paragraph-content">{this.props.data.content}</p>
-                <p className="home-tile--paragraph home-tile--paragraph-click-message">{this.props.data.clickMessage}</p>
-            </div>
+            <Link to={this.props.data.link}>
+                <div className="home-tile--main-div">
+                    <p className="home-tile--paragraph home-tile--paragraph-heading">{this.props.data.title}</p>
+                    <p className="home-tile--paragraph home-tile--paragraph-content">{this.props.data.content}</p>
+                    <p className="home-tile--paragraph home-tile--paragraph-click-message">{this.props.data.clickMessage}</p>
+                </div>
+            </Link>
         );
     }
 }
