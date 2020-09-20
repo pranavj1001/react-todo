0771d1c0d6a7fef917d79a885328c8e20b47049a
Pranav Jain <pranavj1001@gmail.com>
2020-09-20 15:45:00 +0530

    add test cases

--- client/react-todo/src/components/App/App.test.js
+++ client/react-todo/src/components/App/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from '../../reducers';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+const store = createStore(reducers, applyMiddleware(thunk));
+
+test('App is being rendered', () => {
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
+  const linkElement = getByText(/React Todo/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Greeting message being rendered', () => {
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
+  const linkElement = getByText(/Welcome!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Todo tile being rendered', () => {
+  const { getAllByText } = render(<Provider store={store}><App /></Provider>);
+  const linkElement = getAllByText(/Todos/i);
+  expect(linkElement[0]).toBeInTheDocument();
+});
+
+test('Bucket tile is being rendered', () => {
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
+  const linkElement = getByText(/Buckets/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Footer is being rendered', () => {
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
+  const linkElement = getByText(/Pranav Jain/i);
   expect(linkElement).toBeInTheDocument();
 });
